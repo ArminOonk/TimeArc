37644a58b1dab2371bf8eb7c3f15cf17f603e9ee
armin <armin@berkelaarmrt.com>
2014-05-27 22:37:07 +0200

    Hardware pin definitions

--- Firmware/main.cpp
+++ Firmware/main.cpp
@@ -77,15 +77,35 @@ void initGPIO(void)
 
   GPIO_InitTypeDef GPIO_InitStructure; 
   GPIO_StructInit(&GPIO_InitStructure);
-  EXTI_InitTypeDef EXTI_InitStructure; 
-  EXTI_StructInit(&EXTI_InitStructure);
-  NVIC_InitTypeDef NVIC_InitStructure; 
 
-  //GPIO_InitStructure.GPIO_Pin   = STATUS1_PIN;
-  //GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_Out_PP;
-  //GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  //GPIO_Init(STATUS1_PORT, &GPIO_InitStructure);
+  // DOZEN Pins
+  GPIO_InitStructure.GPIO_Pin   = DOZEN1_PIN | DOZEN2_PIN | DOZEN3_PIN | DOZEN4_PIN | DOZEN5_PIN | DOZEN6_PIN;
+  GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_Out_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(DOZEN1_PORT, &GPIO_InitStructure);
 
+  GPIO_InitStructure.GPIO_Pin   = DOZEN7_PIN | DOZEN8_PIN | DOZEN9_PIN | DOZEN10_PIN | DOZEN11_PIN | DOZEN12_PIN;
+  GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_Out_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(DOZEN7_PORT, &GPIO_InitStructure);
+
+  // ARC DOZEN (PWM timer)
+  GPIO_InitStructure.GPIO_Pin   = ARCDOZEN1_PIN | ARCDOZEN2_PIN | ARCDOZEN7_PIN | ARCDOZEN8_PIN | ARCDOZEN9_PIN | ARCDOZEN10_PIN;
+  GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_AF_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(ARCDOZEN1_PORT, &GPIO_InitStructure);
+
+  // ARC DOZEN (PWM timer)
+  GPIO_InitStructure.GPIO_Pin   = ARCDOZEN3_PIN | ARCDOZEN4_PIN | ARCDOZEN5_PIN | ARCDOZEN6_PIN;
+  GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_AF_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(ARCDOZEN3_PORT, &GPIO_InitStructure);
+  
+  // ARC DOZEN (PWM timer)
+  GPIO_InitStructure.GPIO_Pin   = ARCDOZEN11_PIN | ARCDOZEN12_PIN;
+  GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_AF_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(ARCDOZEN11_PORT, &GPIO_InitStructure);
 }
 
 void initTimer(void)
@@ -94,14 +114,87 @@ void initTimer(void)
   TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure; 
   TIM_TimeBaseStructInit(&TIM_TimeBaseStructure);
     
-  // TIM3 is controlle timer @1MHz
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
-  TIM_TimeBaseStructure.TIM_Prescaler         = 72;
-  TIM_TimeBaseStructure.TIM_Period            = 65535;
+  TIM_OCInitTypeDef TIM_OCInitStructure;
+
+  // TIM2
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+
+  TIM_TimeBaseStructure.TIM_Prescaler         = 0;
+  TIM_TimeBaseStructure.TIM_Period            = 255;
   TIM_TimeBaseStructure.TIM_ClockDivision     = TIM_CKD_DIV1;
   TIM_TimeBaseStructure.TIM_CounterMode       = TIM_CounterMode_Up;
+
+  TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = 0;
+  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_Low;
+  TIM_OC1Init(TIM2, &TIM_OCInitStructure);
+  TIM_OC1PreloadConfig(TIM2, TIM_OCPreload_Enable);
+
+  TIM_OC2Init(TIM2, &TIM_OCInitStructure);
+  TIM_OC2PreloadConfig(TIM2, TIM_OCPreload_Enable);
+
+  TIM_OC3Init(TIM2, &TIM_OCInitStructure);
+  TIM_OC3PreloadConfig(TIM2, TIM_OCPreload_Enable);
+  
+  TIM_OC4Init(TIM2, &TIM_OCInitStructure);
+  TIM_OC4PreloadConfig(TIM2, TIM_OCPreload_Enable);
+
+  TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+  TIM_ARRPreloadConfig(TIM2, ENABLE);
+  TIM_Cmd(TIM2, ENABLE); 
+
+  // TIM3
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+
+  TIM_OC1Init(TIM3, &TIM_OCInitStructure);
+  TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Enable);
+
+  TIM_OC2Init(TIM3, &TIM_OCInitStructure);
+  TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable);
+
+  TIM_OC3Init(TIM3, &TIM_OCInitStructure);
+  TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable);
+  
+  TIM_OC4Init(TIM3, &TIM_OCInitStructure);
+  TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable);
+
   TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
+  TIM_ARRPreloadConfig(TIM3, ENABLE);
   TIM_Cmd(TIM3, ENABLE); 
+
+  // TIM4
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+
+  TIM_OC1Init(TIM4, &TIM_OCInitStructure);
+  TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
+
+  TIM_OC2Init(TIM4, &TIM_OCInitStructure);
+  TIM_OC2PreloadConfig(TIM4, TIM_OCPreload_Enable);
+
+  TIM_OC3Init(TIM4, &TIM_OCInitStructure);
+  TIM_OC3PreloadConfig(TIM4, TIM_OCPreload_Enable);
+  
+  TIM_OC4Init(TIM4, &TIM_OCInitStructure);
+  TIM_OC4PreloadConfig(TIM4, TIM_OCPreload_Enable);
+
+  TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
+  TIM_ARRPreloadConfig(TIM4, ENABLE);
+  TIM_Cmd(TIM4, ENABLE);
+
+  // Initial values
+  ARCDOZEN1_PWM(0);
+  ARCDOZEN2_PWM(0);
+  ARCDOZEN3_PWM(0);
+  ARCDOZEN4_PWM(0);
+  ARCDOZEN5_PWM(0);
+  ARCDOZEN6_PWM(0);
+  ARCDOZEN7_PWM(0);
+  ARCDOZEN8_PWM(0);
+  ARCDOZEN9_PWM(0);
+  ARCDOZEN10_PWM(0);
+  ARCDOZEN11_PWM(0);
+  ARCDOZEN12_PWM(0);
 }
 
 void ctl_handle_error(CTL_ERROR_CODE_t error)
