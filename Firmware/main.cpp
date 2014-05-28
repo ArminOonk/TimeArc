96bbd5e99ecb05ef0619b76c863e336ebc43394e
armin <armin@berkelaarmrt.com>
2014-05-28 22:54:45 +0200

    Starting displayBuffer

--- Firmware/main.cpp
+++ Firmware/main.cpp
@@ -5,18 +5,25 @@
 #include <string.h>
 #include <ctl_api.h>
 #include "main.h"
+#include "displayBuffer.h"
 
 //FUNCTIONS
 void ledHandler(void *p);
-void ledsOff();
 void ctl_delay(CTL_TIME_t t);
-
 
 CTL_TASK_t mainTask, ledTask;
 static unsigned ledTaskStack[256];
 
+displayBuffer display;
+
 void ledHandler(void *p)
 {
+  for(int i=0; i< totalNrLeds; i++)
+  {
+    display.ledOn(i);
+  }
+  display.switchBuffer();
+
   while(true)
   {
     ctl_delay(1000);
@@ -66,37 +73,6 @@ void error(const char *err)
   }
 }
 
-void ledsOff()
-{
-  DOZEN1_LOW;
-  DOZEN2_LOW;
-  DOZEN3_LOW;
-  DOZEN4_LOW;
-  DOZEN5_LOW;
-  DOZEN6_LOW;
-  DOZEN7_LOW;
-  DOZEN8_LOW;
-  DOZEN9_LOW;
-  DOZEN10_LOW;
-  DOZEN11_LOW;
-  DOZEN12_LOW;
-
-
-  // ARCDOZEN high leds off
-  ARCDOZEN1_HIGH;
-  ARCDOZEN2_HIGH;
-  ARCDOZEN3_HIGH;
-  ARCDOZEN4_HIGH;
-  ARCDOZEN5_HIGH;
-  ARCDOZEN6_HIGH;
-  ARCDOZEN7_HIGH;
-  ARCDOZEN8_HIGH;
-  ARCDOZEN9_HIGH;
-  ARCDOZEN10_HIGH;
-  ARCDOZEN11_HIGH;
-  ARCDOZEN12_HIGH;
-}
-
 void initGPIO(void)
 {
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
@@ -109,7 +85,7 @@ void initGPIO(void)
 
   GPIO_InitTypeDef GPIO_InitStructure; 
   GPIO_StructInit(&GPIO_InitStructure);
-
+/*
   // DOZEN Pins
   GPIO_InitStructure.GPIO_Pin   = DOZEN1_PIN | DOZEN2_PIN | DOZEN3_PIN | DOZEN4_PIN | DOZEN5_PIN | DOZEN6_PIN;
   GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_Out_PP;
@@ -137,9 +113,7 @@ void initGPIO(void)
   GPIO_InitStructure.GPIO_Pin   = ARCDOZEN11_PIN | ARCDOZEN12_PIN;
   GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_Out_PP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(ARCDOZEN11_PORT, &GPIO_InitStructure);
-
-  ledsOff();
+  GPIO_Init(ARCDOZEN11_PORT, &GPIO_InitStructure);*/
 }
 
 void initTimer(void)
@@ -168,95 +142,6 @@ void initTimer(void)
   TIM_Cmd(TIM2, ENABLE);
   TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
 }
-
-/*void initTimer(void)
-{
-  NVIC_InitTypeDef NVIC_InitStructure;
-  TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure; 
-  TIM_TimeBaseStructInit(&TIM_TimeBaseStructure);
-    
-  TIM_OCInitTypeDef TIM_OCInitStructure;
-
-  // TIM2
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
-
-  TIM_TimeBaseStructure.TIM_Prescaler         = 0;
-  TIM_TimeBaseStructure.TIM_Period            = 255;
-  TIM_TimeBaseStructure.TIM_ClockDivision     = TIM_CKD_DIV1;
-  TIM_TimeBaseStructure.TIM_CounterMode       = TIM_CounterMode_Up;
-
-  TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
-  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-  TIM_OCInitStructure.TIM_Pulse = 0;
-  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_Low;
-  TIM_OC1Init(TIM2, &TIM_OCInitStructure);
-  TIM_OC1PreloadConfig(TIM2, TIM_OCPreload_Enable);
-
-  TIM_OC2Init(TIM2, &TIM_OCInitStructure);
-  TIM_OC2PreloadConfig(TIM2, TIM_OCPreload_Enable);
-
-  TIM_OC3Init(TIM2, &TIM_OCInitStructure);
-  TIM_OC3PreloadConfig(TIM2, TIM_OCPreload_Enable);
-  
-  TIM_OC4Init(TIM2, &TIM_OCInitStructure);
-  TIM_OC4PreloadConfig(TIM2, TIM_OCPreload_Enable);
-
-  TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
-  TIM_ARRPreloadConfig(TIM2, ENABLE);
-  TIM_Cmd(TIM2, ENABLE); 
-
-  // TIM3
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
-
-  TIM_OC1Init(TIM3, &TIM_OCInitStructure);
-  TIM_OC1PreloadConfig(TIM3, TIM_OCPreload_Enable);
-
-  TIM_OC2Init(TIM3, &TIM_OCInitStructure);
-  TIM_OC2PreloadConfig(TIM3, TIM_OCPreload_Enable);
-
-  TIM_OC3Init(TIM3, &TIM_OCInitStructure);
-  TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable);
-  
-  TIM_OC4Init(TIM3, &TIM_OCInitStructure);
-  TIM_OC4PreloadConfig(TIM3, TIM_OCPreload_Enable);
-
-  TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
-  TIM_ARRPreloadConfig(TIM3, ENABLE);
-  TIM_Cmd(TIM3, ENABLE); 
-
-  // TIM4
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
-
-  TIM_OC1Init(TIM4, &TIM_OCInitStructure);
-  TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
-
-  TIM_OC2Init(TIM4, &TIM_OCInitStructure);
-  TIM_OC2PreloadConfig(TIM4, TIM_OCPreload_Enable);
-
-  TIM_OC3Init(TIM4, &TIM_OCInitStructure);
-  TIM_OC3PreloadConfig(TIM4, TIM_OCPreload_Enable);
-  
-  TIM_OC4Init(TIM4, &TIM_OCInitStructure);
-  TIM_OC4PreloadConfig(TIM4, TIM_OCPreload_Enable);
-
-  TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
-  TIM_ARRPreloadConfig(TIM4, ENABLE);
-  TIM_Cmd(TIM4, ENABLE);
-
-  // Initial values
-  ARCDOZEN1_PWM(0);
-  ARCDOZEN2_PWM(0);
-  ARCDOZEN3_PWM(0);
-  ARCDOZEN4_PWM(0);
-  ARCDOZEN5_PWM(0);
-  ARCDOZEN6_PWM(0);
-  ARCDOZEN7_PWM(0);
-  ARCDOZEN8_PWM(0);
-  ARCDOZEN9_PWM(0);
-  ARCDOZEN10_PWM(0);
-  ARCDOZEN11_PWM(0);
-  ARCDOZEN12_PWM(0);
-}*/
 
 void ctl_handle_error(CTL_ERROR_CODE_t error)
 {
