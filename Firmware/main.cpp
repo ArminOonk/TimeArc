0a678b20d4fc5ec99361e559c2b9793f4080aae9
armin <armin@berkelaarmrt.com>
2014-06-04 22:29:06 +0200

    Trying LSE

--- Firmware/main.cpp
+++ Firmware/main.cpp
@@ -10,6 +10,7 @@
 void ledHandler(void *p);
 void ctl_delay(CTL_TIME_t t);
 void initADC();
+void initRTC();
 unsigned int readLight();
 
 CTL_TASK_t mainTask, ledTask, touchTask;
@@ -165,6 +166,7 @@ int main(void)
  
   initGPIO();   // Configure GPIO
   initADC();
+  //initRTC();
 
   // CTL
   ctl_start_timer(ctl_increment_tick_from_isr);
@@ -272,4 +274,77 @@ void ctl_handle_error(CTL_ERROR_CODE_t error)
 void ctl_delay(CTL_TIME_t t)
 {
   ctl_timeout_wait(ctl_get_current_time()+t);
+}
+
+void RTC_Configuration(void)
+{
+  /* Enable PWR and BKP clocks */
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR | RCC_APB1Periph_BKP, ENABLE);
+
+  /* Allow access to BKP Domain */
+  PWR_BackupAccessCmd(ENABLE);
+
+  /* Reset Backup Domain */
+  BKP_DeInit();
+
+  /* Enable LSE */
+  RCC_LSEConfig(RCC_LSE_ON);
+  /* Wait till LSE is ready */
+  while (RCC_GetFlagStatus(RCC_FLAG_LSERDY) == RESET)
+  {}
+
+  /* Select LSE as RTC Clock Source */
+  RCC_RTCCLKConfig(RCC_RTCCLKSource_LSE);
+
+  /* Enable RTC Clock */
+  RCC_RTCCLKCmd(ENABLE);
+
+  /* Wait for RTC registers synchronization */
+  RTC_WaitForSynchro();
+
+  /* Wait until last write operation on RTC registers has finished */
+  RTC_WaitForLastTask();
+
+  /* Enable the RTC Second */
+  RTC_ITConfig(RTC_IT_SEC, ENABLE);
+
+  /* Wait until last write operation on RTC registers has finished */
+  RTC_WaitForLastTask();
+
+  /* Set RTC prescaler: set RTC period to 1sec */
+  RTC_SetPrescaler(32767); /* RTC period = RTCCLK/RTC_PR = (32.768 KHz)/(32767+1) */
+
+  /* Wait until last write operation on RTC registers has finished */
+  RTC_WaitForLastTask();
+}
+
+void initRTC()
+{
+  if (BKP_ReadBackupRegister(BKP_DR1) != 0xA5A5)
+  {
+    RTC_Configuration();
+    // Get the time one way or another
+    BKP_WriteBackupRegister(BKP_DR1, 0xA5A5);
+  }
+
+  // Check if the Power On Reset flag is set */
+  if (RCC_GetFlagStatus(RCC_FLAG_PORRST) != RESET)
+  {
+  }
+  // Check if the Pin Reset flag is set
+  else if (RCC_GetFlagStatus(RCC_FLAG_PINRST) != RESET)
+  {
+  }
+
+  // Wait for RTC registers synchronization
+  RTC_WaitForSynchro();
+
+  //Enable the RTC Second
+  RTC_ITConfig(RTC_IT_SEC, ENABLE);
+
+  //Wait until last write operation on RTC registers has finished
+  RTC_WaitForLastTask();
+
+  // Clear reset flags
+  RCC_ClearFlag();
 }
--- Firmware/stm32f10x_it.cpp
+++ Firmware/stm32f10x_it.cpp
@@ -6,6 +6,21 @@
 #include "capTouch.h"
 #include "adxl345.h"
 
+extern "C" void RTC_IRQHandler(void)
+{
+  if (RTC_GetITStatus(RTC_IT_SEC) != RESET)
+  {
+    //Clear the RTC Second interrupt
+    RTC_ClearITPendingBit(RTC_IT_SEC);
+    
+    // Do stuff
+
+    // Wait for last task
+    RTC_WaitForLastTask();
+    
+  }
+}
+
 extern displayBuffer display;
 
 extern "C" void TIM2_IRQHandler(void)
