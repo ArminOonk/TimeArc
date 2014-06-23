02af17853a5b15269a057c7428244354b1fb0969
armin <armin@berkelaarmrt.com>
2014-06-23 22:06:06 +0200

    First test

--- Software/timearc.py
+++ Software/timearc.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python
-print("Hello, World!")
+import os
+import time
+import serial
+import platform
+
+if(platform.system() == "Windows"):
+    print("Windows detected")
+
+timeArcSerial = serial.Serial("COM5", 230400, timeout=1)
+
+if not timeArcSerial.isOpen():
+    print("Serial port not open, opening ...")
+    timeArcSerial.open()
+    print("Opened")
+else:
+    print("Serial opened: " + timeArcSerial.name )
+
+timeArcSerial.write("TIME?\r\n".encode())
+time.sleep(1)
+response = timeArcSerial.readline()
+print("Response: " + str(response).strip())
+
+timeArcSerial.close()
