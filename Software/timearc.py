0169df04e2ed49854f7c0aec64fd8988028393f8
armin <armin@berkelaarmrt.com>
2014-06-25 22:54:47 +0200

    Moved serial code to its own class

--- Software/timearc.py
+++ Software/timearc.py
@@ -4,67 +4,16 @@ import time
 import serial
 import platform
 import calendar
+import timearcSerial
 
 if(platform.system() == "Windows"):
     print("Windows detected")
 
-timeArcSerial = serial.Serial("COM5", 230400, timeout=1)
+taComm = timearcSerial.TimeArcSerial()
 
-def sendCommand(command):
-    if timeArcSerial.isOpen():
-        print("Sending: " + command)
-        command += "\r\n"
-        timeArcSerial.write(command.encode())
-
-def receiveData():
-    try:
-        response = timeArcSerial.readline()
-    except serial.serialutil.SerialException:
-        print("Serial exception, handle it!")
-
-    txt = str(response.strip(), "ascii")
-    if txt == "":
-        #print("Empty response")
-        return
-    else:
-        print("Response: " + txt)
-        vals = txt.split("=")
-
-        if len(vals) == 2:
-            print("First: " + vals[0] + " Second: " + vals[1])
-            if vals[0] == "TIME":
-                print("Received time from electronics")
-                timeArcTime = int(vals[1])
-                diffTime = int(time.time()) - timeArcTime
-                timeSinceStart = int(time.time())-startTime
-                print("timeArcTime: " + str(timeArcTime) + " computerTime: " + str(int(time.time())) + " diff: " + str(diffTime))
-                print("StartTime: " + str(startTime) + " seconds since start: " + str(timeSinceStart) + " Offset promil: " + str((diffTime/timeSinceStart)*1000.0) )
-
-        else:
-            print("Wrong length")
-
-def setTime():
-    sendCommand("TIME="+str(int(time.time())))
-
-def setOffset():
-    offset = 0
-    if time.daylight != 0:
-        offset =  -time.altzone
-    else:
-        offset = -time.timezone
-
-    print("Offset: " + str(offset))
-    sendCommand("OFFSET="+str(offset))
-
-if not timeArcSerial.isOpen():
-    print("Serial port not open, opening ...")
-    timeArcSerial.open()
-    print("Opened")
-else:
-    print("Serial opened: " + timeArcSerial.name )
-
-setTime()
-setOffset()
+taComm.setTime()
+time.sleep(0.5)
+taComm.setOffset()
 startTime = int(time.time())
 checkInterval = 60
 nextCheckTime = int(time.time()) + 20
@@ -73,11 +22,9 @@ time.sleep(1)
 while True:
     time.sleep(0.1)
     if int(time.time()) > nextCheckTime:
-        sendCommand("TIME?")
-        receiveData()
+        taComm.sendCommand("TIME?")
+        taComm.receiveData()
         nextCheckTime = int(time.time()) + checkInterval
 
-    if timeArcSerial.inWaiting() > 0:
-        receiveData()
+    taComm.receiveData()
 
-timeArcSerial.close()
---
+++ Software/timearcSerial.py
@@ -0,0 +1,58 @@
+import serial
+import time
+
+class TimeArcSerial:
+
+    serialPortName = "COM5"
+
+    try:
+        serialPort = serial.Serial(serialPortName, 230400, timeout=1)
+    except serial.SerialException:
+        print("Error opening serial port")
+
+    def __init__(self):
+        pass
+
+    def sendCommand(self, command):
+        if self.serialPort.isOpen():
+            print("Sending: " + command)
+            command += "\r\n"
+            self.serialPort.write(command.encode())
+
+
+    def setTime(self):
+        self.sendCommand("TIME="+str(int(time.time())))
+
+    def setOffset(self):
+        offset = 0
+        if time.daylight != 0:
+            offset =  -time.altzone
+        else:
+            offset = -time.timezone
+
+        print("Offset: " + str(offset))
+        self.sendCommand("OFFSET="+str(offset))
+
+    def receiveData(self):
+        if self.serialPort.inWaiting() > 0:
+            try:
+                response = self.serialPort.readline()
+            except serial.serialutil.SerialException:
+                print("Serial exception, handle it!")
+
+            txt = str(response.strip(), "ascii")
+            if txt == "":
+                #print("Empty response")
+                return
+            else:
+                print("Response: " + txt)
+                vals = txt.split("=")
+
+                if len(vals) == 2:
+                    print("First: " + vals[0] + " Second: " + vals[1])
+                    if vals[0] == "TIME":
+                        print("Received time from electronics")
+                        timeArcTime = int(vals[1])
+
+                else:
+                    print("Wrong length")
